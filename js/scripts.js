7df47d60717a2f9ffde2ce71ff3d4dc9ca7615a0
Anne Belka and Michael Andrade <anne@>
2016-10-17 10:36:48 -0700

    add intial files

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var add = function(number1, number2){
+	return number1 + number2;
+};
+
+var firstAdd = parseInt(prompt("Enter a number:"));
+var secondAdd = parseInt(prompt("Enter another number:"));
+
+alert(add(firstAdd, secondAdd));
