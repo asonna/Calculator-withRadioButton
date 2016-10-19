51bc3b55a7b1b8cac79d05a5058b3e22784fa64e
Annie Sonna and Michael Andrade <michael@>
2016-10-19 11:48:41 -0700

    add interactive form

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,29 @@
-var add = function(number1, number2){
-	return number1 + number2;
+// Business Logic
+
+var add = function(number1, number2) {
+  return number1 + number2;
 };
 
-var firstAdd = parseInt(prompt("Enter a number:"));
-var secondAdd = parseInt(prompt("Enter another number:"));
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
 
-alert(add(firstAdd, secondAdd));
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+// End Business Logic
+// User Interface
+$(function() {
+	$("form#add").submit(function() {
+		event.preventDefault();
+		var number1 = parseInt($("#add1").val());
+		var number2 = parseInt($("#add2").val());
+		var result = add(number1, number2);
+		$("#output").text(result);
+	});
+});
