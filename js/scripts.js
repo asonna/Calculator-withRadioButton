35d7aedc6365f0d18c4bb4aff9d7780621f50683
Annie Sonna and Michael Andrade <michael@>
2016-10-19 13:48:07 -0700

    add other operations on seperate forms with decimal support

--- js/scripts.js
+++ js/scripts.js
@@ -17,13 +17,34 @@ var divide = function(number1, number2) {
 };
 
 // End Business Logic
-// User Interface
+// User Floaterface
 $(function() {
 	$("form#add").submit(function() {
 		event.preventDefault();
-		var number1 = parseInt($("#add1").val());
-		var number2 = parseInt($("#add2").val());
+		var number1 = parseFloat($("#add1").val());
+		var number2 = parseFloat($("#add2").val());
 		var result = add(number1, number2);
+		$("#output").text(result);
+	});
+	$("form#multiply").submit(function() {
+		event.preventDefault();
+		var number1 = parseFloat($("#multiple1").val());
+		var number2 = parseFloat($("#multiple2").val());
+		var result = multiply(number1, number2);
+		$("#output").text(result);
+	});
+	$("form#subtract").submit(function() {
+		event.preventDefault();
+		var number1 = parseFloat($("#subtract1").val());
+		var number2 = parseFloat($("#subtract2").val());
+		var result = subtract(number1, number2);
+		$("#output").text(result);
+	});
+	$("form#divide").submit(function() {
+		event.preventDefault();
+		var number1 = parseFloat($("#divide1").val());
+		var number2 = parseFloat($("#divide2").val());
+		var result = divide(number1, number2);
 		$("#output").text(result);
 	});
 });
