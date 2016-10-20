1d8c9d5ab190ed16d9c9bf62dd0a8c45821808f1
Annie Sonna and Chris Clifford <annie@>
2016-10-20 15:17:25 -0700

    updated calculator app with radio function and branching

--- js/scripts.js
+++ js/scripts.js
@@ -19,32 +19,24 @@ var divide = function(number1, number2) {
 // End Business Logic
 // User Floaterface
 $(function() {
-	$("form#add").submit(function() {
+	$("form#input").submit(function() {
 		event.preventDefault();
-		var number1 = parseFloat($("#add1").val());
-		var number2 = parseFloat($("#add2").val());
-		var result = add(number1, number2);
+    var userSelection = $("input:radio[name=operator]:checked").val();
+
+		var number1 = parseFloat($("#input1").val());
+		var number2 = parseFloat($("#input2").val());
+
+    if (userSelection === "add") {
+      var result = add(number1, number2);
+    } else if (userSelection === "subtract") {
+      var result = subtract(number1, number2);
+    } else if (userSelection === "multiply") {
+      var result = multiply(number1, number2);
+    } else {
+      var result = divide(number1, number2);
+    }
+
 		$("#output").text(result);
 	});
-	$("form#multiply").submit(function() {
-		event.preventDefault();
-		var number1 = parseFloat($("#multiple1").val());
-		var number2 = parseFloat($("#multiple2").val());
-		var result = multiply(number1, number2);
-		$("#output").text(result);
-	});
-	$("form#subtract").submit(function() {
-		event.preventDefault();
-		var number1 = parseFloat($("#subtract1").val());
-		var number2 = parseFloat($("#subtract2").val());
-		var result = subtract(number1, number2);
-		$("#output").text(result);
-	});
-	$("form#divide").submit(function() {
-		event.preventDefault();
-		var number1 = parseFloat($("#divide1").val());
-		var number2 = parseFloat($("#divide2").val());
-		var result = divide(number1, number2);
-		$("#output").text(result);
-	});
+
 });
